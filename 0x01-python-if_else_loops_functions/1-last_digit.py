fd9645dd97cc1911ac73aaec89dc81bcdbe605f8
root <root@DESKTOP-T7E1AKA.localdomain>
2023-08-09 13:55:58 +0200

    pushing 1-last_digit task file

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+last_digit = number % 10
+message = f"Last digit of {number} is {last_digit}"
+if last_digit > 5:
+    print(f"{message} and is greater than 5".format(number, last_digit))
+elif last_digit == 0:
+    print(f"{message} and is 0".format(number, last_digit))
+else:
+    print(f"{message} and is less than 6 and not 0".format(number, last_digit))
