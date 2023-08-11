b042bd55dd131af7466d20eed58137b98a6052a1
root <root@DESKTOP-T7E1AKA.localdomain>
2023-08-11 03:19:05 +0200

    push the rest of the files once

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
-a = 1
-b = 2
-
-
-def add():
-    return (a + b)
-
-
-print("{} + {} = {}".format(a, b, add()), end="\n")
+from add_0 import add
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from hidden_4 import hidden_4
+    d = dir()
+    for i in range(0, len(d)):
+        if d[i][0:2] != "__":
+            print("{}".format(d[i]))
