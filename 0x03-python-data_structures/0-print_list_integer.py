2dd039dcb98f4f845561428f2091182c99b989fe
root <root@DESKTOP-T7E1AKA.localdomain>
2023-08-12 17:18:35 +0200

    update

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-        list = [my_list]
-        for i in range(0, len(list)):
-            print(list[i])
+        for i in range(0, len(my_list)):
+            print(my_list[i])
