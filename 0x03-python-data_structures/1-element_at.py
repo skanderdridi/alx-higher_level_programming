29c73f69a7fc1b0e0198413694b63a68cda89f62
root <root@DESKTOP-T7E1AKA.localdomain>
2023-08-13 15:14:11 +0200

    update 3-element.py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx > 0:
-       if idx < len(my_list):
-           return my_list[idx]
-       else:
+    if idx < 0:
+       if idx > len(my_list):
            return
+    else:
+        return my_list[idx]
