1ba8d770958f166be06198f3b4a44deceb3507da
root <root@DESKTOP-T7E1AKA.localdomain>
2023-08-13 17:16:17 +0200

    push 6-print_matrix task files

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for i in matrix:
+        for n in i:
+            print("{:d}".format(n), end="")
+            if(n is not i[len(i)-1]):
+                print(" ", end="")
+        print()
