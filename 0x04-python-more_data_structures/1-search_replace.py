aad95182a4e900ceab1786c2339d02ebe9cf989c
Skander <62188738+skanderdridi@users.noreply.github.com>
2023-08-17 02:16:16 +0100

    Update 1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,1 +1,7 @@
-
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    new_list = my_list.copy()
+    for item in new_list:
+        if item == search:
+            new_list[new_list.index(item)] = replace
+    return new_list
